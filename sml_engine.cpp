5fc63da400d67b688dbb96edd20b0c124f11fff4
Guillaume Roy <royguillaume2005@icloud.com>
2025-06-30 13:49:47 -0400

    add imgui integration

--- sml_engine.cpp
+++ sml_engine.cpp
@@ -15,8 +15,14 @@ typedef float sml_f32;
 #define Sml_Megabytes(Amount) (Kilobytes(Amount) * 1024)
 #define Sml_Gigabytes(Amount) (Megabytes(Amount) * 1024)
 
+enum class sml_instance  : sml_u32 {};
+enum class sml_instanced : sml_u32 {};
+
 #define STB_IMAGE_IMPLEMENTATION
 #include "third_party/stb_image.h"
+
+#define IMGUI_DISABLE_CHECKVERSION
+#include "../code/third_party/imgui/imgui.h"
 
 #pragma warning(push)
 #pragma warning(disable: 4505) // Unreferenced functions
@@ -24,6 +30,9 @@ typedef float sml_f32;
 // Math
 #include "math/vector.cpp"
 #include "math/matrix.cpp"
+
+// Spatial
+#include "spatial/entity_test.cpp"
 
 // Rendering
 #include "platform/sml_platform.cpp"
